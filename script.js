b828598d059036fe86b06610347445a3a220df13
Hanaif <hanaif1@gmail.com>
2020-05-07 20:33:25 +0100

    some changes

--- script.js
+++ script.js
@@ -16,18 +16,52 @@ window.onload = setup;
 // console.log(hanif);
 
 function makePageForEpisodes(episodeList) {
-    let rootElem = document.getElementById("root");
+    var rootElem = document.getElementById("root");
     //rootElem.textContent = `Got ${episodeList[0].id} episode(s)`;
-    for (let i = 0; i < episodeList.length; i++) {
-        let element = document.createElement("h1");
-        let image = document.createElement("img");
-        let text = document.createElement("p");
+    for (var i = 0; i < episodeList.length; i++) {
+        var container = document.createElement("div");
+        var element = document.createElement("h4");
+        var image = document.createElement("img");
+        var text = document.createElement("p");
         element.textContent = `${episodeList[i].name} - S0${episodeList[i].season}E0${episodeList[i].number}`;
         image.src = episodeList[i].image.medium;
-        text.textContent = episodeList[i].summary;
-        rootElem.appendChild(element);
-        rootElem.appendChild(image);
-        rootElem.appendChild(text);
+        text.innerHTML = `${
+        episodeList[i].summary.length < 50
+          ? episodeList[i].summary
+          : episodeList[i].summary.substring(0, 200)
+      }....`;
+        element.classList.add("title");
+        image.classList.add("image");
+        text.classList.add("text");
+        container.classList.add("container");
+
+        container.appendChild(element);
+        container.appendChild(image);
+        container.appendChild(text);
+        rootElem.appendChild(container);
     }
+
     return rootElem;
 }
+var filter, main, container, title, text, i, txtValue, pValue;
+var input = document.getElementById("myInput");
+input.addEventListener("keyup", function myFunction() {
+    filter = input.value.toUpperCase();
+
+    main = document.getElementById("root");
+    container = main.getElementsByTagName("div");
+    for (i = 0; i < container.length; i++) {
+        title = container[i].getElementsByTagName("h4")[0];
+        text = container[i].getElementsByTagName("p")[0];
+        txtValue = title.textContent || title.innerText;
+        pValue = text.textContent || text.innerText;
+        if (
+            txtValue.toUpperCase().indexOf(filter) > -1 ||
+            pValue.toUpperCase().indexOf(filter) > -1
+        ) {
+            container[i].style.display = "";
+        } else {
+            container[i].style.display = "none";
+        }
+    }
+});
